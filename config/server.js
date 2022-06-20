5d02cd08c4ba65aa64945538770f5789b06d250f
Carlos Tello <ingcarlostello@hotmail.com>
2022-06-19 19:02:33 -0500

    backend restaurante

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aef8a681ff3d23a6251a911ba032d18c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e9f3da71-651a-4af2-a5a1-7ebee33498b3'
+};
